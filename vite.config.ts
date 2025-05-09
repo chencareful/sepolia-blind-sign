4f5b5ed9b31864e464c09a67a0033054a0da737e
chencareful <chencareful@gmail.com>
2025-05-09 08:12:01 +0700

    更改路由

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/sepolia-blind-sign/",  // 👈 这里使用你的 GitHub 仓库名
+  base: "./",  // 👈 这里使用你的 GitHub 仓库名
   plugins: [react()],
   build: {
     rollupOptions: {
