71ab047e59a1e0c298c0df2665aa1b9cdd322caa
chencareful <chencareful@gmail.com>
2025-05-10 07:18:08 +0700

    vite.config修改路由为根目录

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/sepolia-blind-sign/' : '/',
+  base: "./", 
   plugins: [react()],
   resolve: {
     alias: {
