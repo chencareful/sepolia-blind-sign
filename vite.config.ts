f1860fafac318fda59a5a0b32f46f46326ba0362
chencareful <chencareful@gmail.com>
2025-05-10 13:05:12 +0700

    vite.config.js   outDir: "dist",

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "", 
+  base: process.env.NODE_ENV === "production" ? "/sepolia-blind-sign/" : "/",  // 关键修改：确保 base 在生产环境正确
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,9 +13,16 @@ export default defineConfig({
     }
   },
   build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    chunkSizeWarningLimit: 1600, // 避免大型资源警告
     rollupOptions: {
-      input: {
-        main: 'index.html'
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        }
       }
     }
   }
